d2152f982fbc8f71cf1cb80097c57a069c8c6528
sondos1470 <sondos.mahmoud1470@gmail.com>
2023-07-15 18:42:30 +0200

    prplem solves

---
+++ map.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  Map<String,String>date=
+  {
+    'name':'sondos mahmoud',
+    'adress':'Giza,Egypt',
+    'age':'21'
+  };
+  print(date);
+
+Map<String,int>Num={
+ 'sondos':35,'eman':40,'mona':23
+};
+print(Num);
+
+
+
+}
