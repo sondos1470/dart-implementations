627a80e9eccd3ea48da08337d8e9050ddbe05533
sondos1470 <sondos.mahmoud1470@gmail.com>
2023-07-18 16:31:18 +0200

    choose your coffe

---
+++ choose-your-coffe-with-if.dart
@@ -0,0 +1,18 @@
+void main()
+{
+  int code =1;
+  String type;
+  if(code == 1)
+  {
+    type='French coffe';
+  }else if(code ==2){
+    type ='Espresso coffe';
+  }else if(code ==3)
+  {
+    type='turkish coffe';
+  }else 
+  {
+   type ='sorry,try again';   
+  }
+  print(type);
+}
---
+++ var&dynamic.dart
@@ -0,0 +1,9 @@
+void main()
+{
+int a=50;
+int s=5;
+var w;
+w=a/s;
+print(w);
+
+}
