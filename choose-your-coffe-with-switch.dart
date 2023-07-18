6587af3d8d8d4a7acb31c2f18630c9c7ae12a57a
sondos1470 <sondos.mahmoud1470@gmail.com>
2023-07-18 17:00:56 +0200

    choose your coffe with switch

---
+++ choose-your-coffe-with-switch.dart
@@ -0,0 +1,19 @@
+void main()
+{
+int code=2;
+switch(code)
+{
+  case 1:
+  print ('French Coffe');
+  break;
+  case 2:
+  print ('Esspresso');
+  break;
+  case 3:
+  print ('turkish');
+  break;
+  default:
+print('sorry try again');
+}
+
+}
